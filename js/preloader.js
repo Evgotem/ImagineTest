e2caf12ad71f9390b2f5006975d1f89387db5a7b
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-03-15 22:15:33 +0400

    подготовил почву для прелоадера

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+// $(window).on('load', function(){
+
+//   $('#preloader').fadeOut();
+
+//   $('#page-preloader').delay(350).fadeOut('slow');
+
+//   $('body').delay(350).css({"overflow": "visible"});
+
+// });
+
