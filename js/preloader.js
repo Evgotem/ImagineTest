445fda312c15daf57dcb134a80048f98ebef222d
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-03-16 16:19:22 +0400

    написал код для прелоадера, который не зависит от скорости загрузки страницы, но работает

--- js/preloader.js
+++ js/preloader.js
@@ -1,10 +1,22 @@
-// $(window).on('load', function(){
+$(document).ready(function () {
+  let numb_start = $("#counter").text(); // Получаем начальное число
+		
+		$({numberValue: numb_start}).animate({numberValue: 100}, {
+		
+			duration: 7000, // Продолжительность анимации
+			easing: "swing",
+			
+			step: function(val) {
+			
+				$("#counter").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+				
+      }			
+      
+    });
 
-//   $('#preloader').fadeOut();
-
-//   $('#page-preloader').delay(350).fadeOut('slow');
-
-//   $('body').delay(350).css({"overflow": "visible"});
-
-// });
+    setTimeout(function () {
+      $('.preloader').addClass('done')
+      }, 7000); 
+      
+});
 
